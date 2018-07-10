d26f7cca9f48270c5a35dd72c36fb594eff608e0
Mathias Methner <Mathias.Methner@otto.de>
2018-07-10 11:32:38 +0200

    re-add qa-ldap-plugin

--- portal.sh
+++ portal.sh
@@ -12,7 +12,7 @@ set -e
 #
 # install qa-lang-de
 #
-rm -rf ./qa-lang/de
+rm -rf ./qa-lang/de /tmp/qa-lang-de
 git clone -b portal --depth 1 https://github.com/hiseke/q2a-lang-de.git /tmp/qa-lang-de
 mv /tmp/qa-lang-de/qa-lang/de/ ./qa-lang/
 rm -rf /tmp/qa-lang-de
@@ -20,16 +20,16 @@ rm -rf /tmp/qa-lang-de
 #
 # install ldap plugin
 #
-rm -rf ./qa-plugin/qa-ldap-login
+rm -rf ./qa-plugin/qa-ldap-login /tmp/qa-ldap-login
 git clone -b master --depth 1 https://github.com/zakkak/qa-ldap-login /tmp/qa-ldap-login
 rm -rf /tmp/qa-ldap-login/.git
-mv /tmp//tmp/qa-ldap-login ./qa-plugin/
+mv /tmp/qa-ldap-login ./qa-plugin/
 rm -rf /tmp/qa-ldap-login
 
 #
 # patch css
 #
-rm -rf ./qa-theme/TwentyTwelve
+rm -rf ./qa-theme/TwentyTwelve /tmp/Q2A-TwentyTwelve
 git clone -b master --depth 1 https://github.com/q2a-projects/Q2A-TwentyTwelve.git /tmp/Q2A-TwentyTwelve
 mv /tmp/Q2A-TwentyTwelve/TwentyTwelve ./qa-theme/
 rm -rf /tmp/Q2A-TwentyTwelve
