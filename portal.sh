a53cdc61258d32133f7ec872577e345e0fbcee66
Mathias Methner <Mathias.Methner@otto.de>
2018-07-10 11:01:49 +0200

    install qa-ldap-login plugin

--- portal.sh
+++ portal.sh
@@ -8,7 +8,18 @@ set -e
 # scripts automates some setup for private otto it-bi-rt
 # question2answer instance
 #
+
+#
+# install qa-lang-de
+#
 git clone -b portal --depth 1 https://github.com/hiseke/q2a-lang-de.git /tmp/qa-lang-de
 mv /tmp/qa-lang-de/qa-lang/de/ ./qa-lang/
 rm -rf /tmp/qa-lang-de
 
+#
+# install ldap plugin
+#
+git clone -b master --depth 1 https://github.com/zakkak/qa-ldap-login /tmp/qa-ldap-login
+rm -rf /tmp/qa-lang-de/.git
+mv /tmp//tmp/qa-ldap-login ./qa-plugin/
+rm -rf /tmp/qa-ldap-login
